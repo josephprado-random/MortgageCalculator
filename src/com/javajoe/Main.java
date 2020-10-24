ebbb2e3dee03eb23a7296def04d6a4c716917bf5
josephprado <pradodje@gmail.com>
2020-10-23 22:36:47 -0700

    refactoring, added readInput method

--- src/com/javajoe/Main.java
+++ src/com/javajoe/Main.java
@@ -7,57 +7,39 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-        double principal;
-        float annualRate;
-        float termYears;
-
         System.out.println("Mortgage Calculator");
-        Scanner scanner = new Scanner(System.in); //creates Scanner object scanner
 
-        //prompts user for loan principal amount > 0
-        while (true) {
-            System.out.print("Loan Amount: ");
-            principal = scanner.nextDouble();
-            if (principal > 0)
-                break;
-            System.out.println("Amount must be greater than 0!");
-        }
+        double principal = readInput ("Loan Amount", 0);
+        float annualRate = (float) readInput("Annual Interest Rate (APR)", 0);
+        float termYears = (float) readInput("Term (years)", 1);
 
-        //prompts user for APR >= 0
-        while (true) {
-            System.out.print("Annual Interest Rate (APR): ");
-            annualRate = scanner.nextFloat();
-            if (annualRate >= 0)
-                break;
-            System.out.println("Rate cannot be negative!");
-        }
-
-        //prompts user for loan term in years > 0
-        while (true) {
-            System.out.print("Term (years): ");
-            termYears = scanner.nextFloat();
-            if (termYears > 0)
-                break;
-            System.out.println("Term must be greater than 0!");
-        }
-
-        //calls calculateMortgage method and formats to currency
         double monthlyPayment = getMonthlyPayment(principal, annualRate, termYears);
+
         NumberFormat paymentFormatted = NumberFormat.getCurrencyInstance();
         String paymentString = paymentFormatted.format(monthlyPayment);
         System.out.println("Monthly Payment: " + paymentString);
-
     }
 
-    public static double getMonthlyPayment(
-            double principal,
-            float annualRate,
-            float termYears) {
+    public static double readInput (String prompt, double min) {
 
-        final byte MONTHS_IN_YEAR = 12; //sets months in year as constant
-        final byte PERCENT = 100; //sets denominator for interest rate (PERCENT)
+        Scanner scanner = new Scanner(System.in);
+        double value;
 
+        //prompts user for input and stores as value
+        while (true) {
+            System.out.print(prompt + ": ");
+            value = scanner.nextDouble();
+            if (value >= min)
+                break;
+            System.out.println("Value must be >= " + (int)min + "!");
+        }
+        return value;
+    }
+
+    public static double getMonthlyPayment(double principal, float annualRate, float termYears) {
+
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
         float monthlyRate = annualRate / PERCENT / MONTHS_IN_YEAR;
         float numberOfPayments = termYears * MONTHS_IN_YEAR;
 
@@ -69,6 +51,5 @@ public class Main {
                 * monthlyRate
                 * Math.pow((1 + monthlyRate), numberOfPayments)
                 / (Math.pow((1 + monthlyRate), numberOfPayments) - 1);
-
     }
 }
